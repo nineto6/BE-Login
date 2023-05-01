e2b1b9733eb2aca622644a0d228e1c7656e6dbf0
chang-seop <ikavon@naver.com>
2023-05-02 03:01:41 +0900

    ADD: UserDto, UserDetailsDto
    Model

---
+++ Login/src/main/java/hello/Login/model/UserDetailsDto.java
@@ -0,0 +1,56 @@
+package hello.Login.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.Delegate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Slf4j
+@Getter
+@AllArgsConstructor
+public class UserDetailsDto implements UserDetails {
+
+    @Delegate
+    /* @Delegate : UserDto 의 메서드가 위임되어서 UserDetailsDto 에서 바로 호출이 가능 */
+    private UserDto userDto;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return userDto.getUserPw();
+    }
+
+    @Override
+    public String getUsername() {
+        return userDto.getUserNm();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
