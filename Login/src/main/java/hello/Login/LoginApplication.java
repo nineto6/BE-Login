4e2b00784b7e56eef400a7d65fa26a47875d2cf5
chang-seop <ikavon@naver.com>
2023-05-07 05:23:22 +0900

    ADD: SSL

--- Login/src/main/java/hello/Login/LoginApplication.java
+++ Login/src/main/java/hello/Login/LoginApplication.java
@@ -2,8 +2,10 @@ package hello.Login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:ssl-local.properties")
 public class LoginApplication {
 
 	public static void main(String[] args) {
