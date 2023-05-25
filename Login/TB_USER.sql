ea36feba56c6891af3e6fbeebef077154b01f78e
chang-seop <ikavon@naver.com>
2023-05-26 07:43:05 +0900

    MODIFY: TB_USER.sql

--- Login/TB_USER.sql
+++ Login/TB_USER.sql
@@ -3,7 +3,7 @@ drop table tb_user cascade;
 create table tb_user(
    user_sq        int auto_increment primary key,
    user_id         varchar(20) not null,
-   user_pw       varchar(20) not null,
+   user_pw       varchar(60) not null,
    user_nm       varchar(20) not null,
    user_st         varchar(1) not null
 );
