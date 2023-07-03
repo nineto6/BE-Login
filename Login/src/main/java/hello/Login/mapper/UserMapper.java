4b44806297ad73da3a2800f8222ce63992035ba0
chang-seop <ikavon@naver.com>
2023-07-04 05:00:15 +0900

    UPDATE: UserMapper, UserMapper.xml, UserMapperTest

--- Login/src/main/java/hello/Login/mapper/UserMapper.java
+++ Login/src/main/java/hello/Login/mapper/UserMapper.java
@@ -1,12 +1,15 @@
 package hello.Login.mapper;
 
+import hello.Login.model.AuthorityDto;
 import hello.Login.model.UserDto;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
 import java.util.Optional;
 
 @Mapper
 public interface UserMapper {
     void save(UserDto userDto);
-    Optional<UserDto> login(UserDto userDto);
+    void insertAuthority(List<AuthorityDto> authorities);
+    Optional<UserDto> findByUserId(String userId);
 }
--- Login/src/test/java/hello/Login/mapper/UserMapperTest.java
+++ Login/src/test/java/hello/Login/mapper/UserMapperTest.java
@@ -1,18 +1,20 @@
 package hello.Login.mapper;
 
+import hello.Login.model.AuthorityDto;
+import hello.Login.model.codes.Role;
 import hello.Login.model.UserDto;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -37,9 +39,45 @@ class UserMapperTest {
         log.info("userSq = {}", user.getUserSq());
 
         // then
-        Optional<UserDto> login = userMapper.login(user);
+        Optional<UserDto> login = userMapper.findByUserId("hello123");
 
         log.info("login is empty = {}", login.isEmpty());
         assertThat(login.isEmpty()).isFalse();
+    }
+
+    @Test
+    @DisplayName("권한 저장 2개 테스트")
+    void insertListOfAuthority() {
+        // given
+        UserDto user = UserDto.builder()
+                .userId("hello123")
+                .userPw("123123")
+                .userNm("헬로")
+                .userSt("X")
+                .build();
+
+        userMapper.save(user);
+
+        ArrayList<AuthorityDto> auth = new ArrayList<>();
+        auth.add(AuthorityDto.builder()
+                .userId("hello123")
+                .userSq(user.getUserSq())
+                .userAuthority(Role.USER.getAuth()).build());
+
+        auth.add(AuthorityDto.builder()
+                .userId("hello123")
+                .userSq(user.getUserSq())
+                .userAuthority(Role.ADMIN.getAuth()).build());
+
+        // when
+        userMapper.insertAuthority(auth);
+
+        // then userId 로 조회 후 권한 존재 유무 확인
+        Optional<UserDto> login = userMapper.findByUserId("hello123");
+        List<String> userRoles = login.get().getUserRoles();
+
+        assertThat(userRoles.size()).isEqualTo(2);
+        assertThat(userRoles.contains(Role.USER.getAuth())).isTrue();
+        assertThat(userRoles.contains(Role.ADMIN.getAuth())).isTrue();
     }
 }
